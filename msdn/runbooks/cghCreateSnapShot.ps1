37f6df3adec1ab1aa51190c82fdc1f0295d27433
Chris Gibson <ChrisGibson1982@users.noreply.github.com>
2018-05-17 22:31:38 +0100

    Updated cghCreateSnapShot.ps1 in Azure Automation.

--- msdn/runbooks/cghCreateSnapShot.ps1
+++ msdn/runbooks/cghCreateSnapShot.ps1
@@ -1,14 +1,41 @@
 Param(
-[string]$StorageAccountName,
-[string]$ContainerName,
-[string]$BlobName
+[Parameter(Mandatory=$true)][string]$StorageAccountName,
+[Parameter(Mandatory=$true)][string]$StorageAccountRG,
+[Parameter(Mandatory=$true)][string]$ContainerName,
+[Parameter(Mandatory=$true)][string]$BlobName
 )
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
 
 $StorageAccountKey = (Get-AzureStorageKey -StorageAccountName $StorageAccountName).Primary
 
-$Ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
-$blob = Get-AzureStorageBlob -Context $Ctx -Container $ContainerName -Blob $BlobName 
-$snap = $blob.ICloudBlob.CreateSnapshot()
 
-Get-AzureStorageBlob –Context $Ctx -Prefix $BlobName -Container $ContainerName| where-Object {$_.ICloudBlob.IsSnapshot -and $_.Name -eq $BlobName -and $_.SnapshotTime -ne $null } | Format-table -autosize -wrap
-# 
+ $Ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+ $blob = Get-AzureStorageBlob -Context $Ctx -Container $ContainerName -Blob $BlobName 
+
+ $snap = $blob.ICloudBlob.CreateSnapshot()
+
+ Get-AzureStorageBlob –Context $Ctx -Prefix $BlobName -Container $ContainerName| where-Object {$_.ICloudBlob.IsSnapshot -and $_.Name -eq $BlobName -and $_.SnapshotTime -ne $null } | Format-table -autosize -wrap
