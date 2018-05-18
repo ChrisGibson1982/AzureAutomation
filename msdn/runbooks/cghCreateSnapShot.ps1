1452f1766af5fa8cf939b9d8e260938f93635c32
Chris Gibson <ChrisGibson1982@users.noreply.github.com>
2018-05-18 13:31:59 +0100

    Updated cghCreateSnapShot.ps1 in Azure Automation.

--- msdn/runbooks/cghCreateSnapShot.ps1
+++ msdn/runbooks/cghCreateSnapShot.ps1
@@ -45,6 +45,6 @@ $Ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageA
 $blob = Get-AzureStorageBlob -Context $Ctx -Container $ContainerName -Blob $BlobName 
 
 $snap = $blob.ICloudBlob.CreateSnapshot()
-$snap.SnapshotQualifiedUri
+$snap
 
 # Get-AzureStorageBlob –Context $Ctx -Prefix $BlobName -Container $ContainerName| where-Object {$_.ICloudBlob.IsSnapshot -and $_.Name -eq $BlobName -and $_.SnapshotTime -ne $null } | Format-table -autosize -wrap
